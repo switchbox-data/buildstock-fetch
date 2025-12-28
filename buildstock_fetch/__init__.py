c2988d86274955368918f7040e7b1dc1a6bb98be
sherryzuo <145484385+sherryzuo@users.noreply.github.com>
2025-12-28 14:39:18 -0800

    add scenario validation and uniform adoption tests

--- buildstock_fetch/__init__.py
+++ buildstock_fetch/__init__.py
@@ -1,1 +1,9 @@
+from buildstock_fetch.scenarios import InvalidScenarioError, uniform_adoption, validate_scenario
+
 __version__ = "1.4.7"
+
+__all__ = [
+    "InvalidScenarioError",
+    "uniform_adoption",
+    "validate_scenario",
+]
