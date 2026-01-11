091aee6a531e45706773e02a1b3f05436760db9f
Alex Lee <lee3742@purdue.edu>
2026-01-11 01:45:40 +0000

    Fix tests for SB upgrades

--- tests/test_SB_upgrade.py
+++ tests/test_SB_upgrade.py
@@ -4,87 +4,136 @@ from pathlib import Path
 sys.path.append(str(Path(__file__).parent.parent))
 
 from buildstock_fetch.building import BuildingID
+from buildstock_fetch.main import fetch_bldg_data
 
 
 def test_SB_upgrade_load_curve_15min():
-    bldg_id = BuildingID(bldg_id=7, upgrade_id="17")
+    bldg_ids = [
+        BuildingID(
+            bldg_id=361520,
+            release_number="2",
+            release_year="2024",
+            res_com="resstock",
+            weather="tmy3",
+            state="IN",
+            upgrade_id="17",
+        )
+    ]
+    file_type = ("load_curve_15min",)
     output_dir = Path("data")
-    output_file = (
-        output_dir
-        / bldg_id.get_release_name()
-        / "load_curve_15min"
-        / f"state={bldg_id.state}"
-        / f"upgrade={str(int(bldg_id.upgrade_id)).zfill(2)}"
-        / f"bldg{str(bldg_id.bldg_id).zfill(7)}-up{str(int(bldg_id.upgrade_id)).zfill(2)}.parquet"
-    )
-    assert output_file.exists()
+
+    _, _ = fetch_bldg_data(bldg_ids, file_type, output_dir)
+    bldg_id = bldg_ids[0]
+    assert Path(
+        f"data/{bldg_id.get_release_name()}/load_curve_15min/state={bldg_id.state}/upgrade={str(int(bldg_id.upgrade_id)).zfill(2)}/{str(bldg_id.bldg_id)!s}-{int(bldg_id.upgrade_id)!s}.parquet"
+    ).exists()
 
 
 def test_SB_upgrade_load_curve_hourly():
-    bldg_id = BuildingID(bldg_id=7, upgrade_id="17")
+    bldg_ids = [
+        BuildingID(
+            bldg_id=361520,
+            release_number="2",
+            release_year="2024",
+            res_com="resstock",
+            weather="tmy3",
+            state="IN",
+            upgrade_id="17",
+        )
+    ]
+    file_type = ("load_curve_hourly",)
     output_dir = Path("data")
-    output_file = (
-        output_dir
-        / bldg_id.get_release_name()
-        / "load_curve_hourly"
-        / f"state={bldg_id.state}"
-        / f"upgrade={str(int(bldg_id.upgrade_id)).zfill(2)}"
-        / f"bldg{str(bldg_id.bldg_id).zfill(7)}-up{str(int(bldg_id.upgrade_id)).zfill(2)}.parquet"
-    )
-    assert output_file.exists()
+
+    _, _ = fetch_bldg_data(bldg_ids, file_type, output_dir)
+    bldg_id = bldg_ids[0]
+    assert Path(
+        f"data/{bldg_id.get_release_name()}/load_curve_hourly/state={bldg_id.state}/upgrade={str(int(bldg_id.upgrade_id)).zfill(2)}/{str(bldg_id.bldg_id)!s}-{int(bldg_id.upgrade_id)!s}.parquet"
+    ).exists()
 
 
 def test_SB_upgrade_load_curve_daily():
-    bldg_id = BuildingID(bldg_id=7, upgrade_id="17")
+    bldg_ids = [
+        BuildingID(
+            bldg_id=361520,
+            release_number="2",
+            release_year="2024",
+            res_com="resstock",
+            weather="tmy3",
+            state="IN",
+            upgrade_id="17",
+        )
+    ]
+    file_type = ("load_curve_daily",)
     output_dir = Path("data")
-    output_file = (
-        output_dir
-        / bldg_id.get_release_name()
-        / "load_curve_daily"
-        / f"state={bldg_id.state}"
-        / f"upgrade={str(int(bldg_id.upgrade_id)).zfill(2)}"
-        / f"bldg{str(bldg_id.bldg_id).zfill(7)}-up{str(int(bldg_id.upgrade_id)).zfill(2)}.parquet"
-    )
-    assert output_file.exists()
+
+    _, _ = fetch_bldg_data(bldg_ids, file_type, output_dir)
+    bldg_id = bldg_ids[0]
+    assert Path(
+        f"data/{bldg_id.get_release_name()}/load_curve_daily/state={bldg_id.state}/upgrade={str(int(bldg_id.upgrade_id)).zfill(2)}/{str(bldg_id.bldg_id)!s}-{int(bldg_id.upgrade_id)!s}.parquet"
+    ).exists()
 
 
 def test_SB_upgrade_load_curve_monthly():
-    bldg_id = BuildingID(bldg_id=7, upgrade_id="17")
+    bldg_ids = [
+        BuildingID(
+            bldg_id=361520,
+            release_number="2",
+            release_year="2024",
+            res_com="resstock",
+            weather="tmy3",
+            state="IN",
+            upgrade_id="17",
+        )
+    ]
+    file_type = ("load_curve_monthly",)
     output_dir = Path("data")
-    output_file = (
-        output_dir
-        / bldg_id.get_release_name()
-        / "load_curve_monthly"
-        / f"state={bldg_id.state}"
-        / f"upgrade={str(int(bldg_id.upgrade_id)).zfill(2)}"
-        / f"bldg{str(bldg_id.bldg_id).zfill(7)}-up{str(int(bldg_id.upgrade_id)).zfill(2)}.parquet"
-    )
-    assert output_file.exists()
+
+    _, _ = fetch_bldg_data(bldg_ids, file_type, output_dir)
+    bldg_id = bldg_ids[0]
+    assert Path(
+        f"data/{bldg_id.get_release_name()}/load_curve_monthly/state={bldg_id.state}/upgrade={str(int(bldg_id.upgrade_id)).zfill(2)}/{str(bldg_id.bldg_id)!s}-{int(bldg_id.upgrade_id)!s}.parquet"
+    ).exists()
 
 
 def test_SB_upgrade_annual_load_curve():
-    bldg_id = BuildingID(bldg_id=7, upgrade_id="17")
+    bldg_ids = [
+        BuildingID(
+            bldg_id=361520,
+            release_number="2",
+            release_year="2024",
+            res_com="resstock",
+            weather="tmy3",
+            state="IN",
+            upgrade_id="17",
+        )
+    ]
+    file_type = ("load_curve_annual",)
     output_dir = Path("data")
-    output_file = (
-        output_dir
-        / bldg_id.get_release_name()
-        / "load_curve_annual"
-        / f"state={bldg_id.state}"
-        / f"upgrade={str(int(bldg_id.upgrade_id)).zfill(2)}"
-        / f"bldg{str(bldg_id.bldg_id).zfill(7)}-up{str(int(bldg_id.upgrade_id)).zfill(2)}.parquet"
-    )
-    assert output_file.exists()
+
+    _, _ = fetch_bldg_data(bldg_ids, file_type, output_dir)
+    bldg_id = bldg_ids[0]
+    assert Path(
+        f"data/{bldg_id.get_release_name()}/load_curve_annual/state={bldg_id.state}/upgrade={str(int(bldg_id.upgrade_id)).zfill(2)}/{str(bldg_id.bldg_id)!s}-{int(bldg_id.upgrade_id)!s}.parquet"
+    ).exists()
 
 
 def test_SB_upgrade_metadata():
-    bldg_id = BuildingID(bldg_id=7, upgrade_id="17")
+    bldg_ids = [
+        BuildingID(
+            bldg_id=361520,
+            release_number="2",
+            release_year="2024",
+            res_com="resstock",
+            weather="tmy3",
+            state="IN",
+            upgrade_id="17",
+        )
+    ]
+    file_type = ("metadata",)
     output_dir = Path("data")
-    output_file = (
-        output_dir
-        / bldg_id.get_release_name()
-        / "metadata"
-        / f"state={bldg_id.state}"
-        / f"upgrade={str(int(bldg_id.upgrade_id)).zfill(2)}"
-        / "metadata.parquet"
-    )
-    assert output_file.exists()
+
+    _, _ = fetch_bldg_data(bldg_ids, file_type, output_dir)
+    bldg_id = bldg_ids[0]
+    assert Path(
+        f"data/{bldg_id.get_release_name()}/metadata/state={bldg_id.state}/upgrade={str(int(bldg_id.upgrade_id)).zfill(2)}/metadata.parquet"
+    ).exists()
